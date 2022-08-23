4277b00e25b91ae322e8e5da79e2c234e095a941
shoaib-ks <shoaib123>
2022-08-23 22:40:43 +0500

    Added OrderController.java

---
+++ src/main/java/com/kalsym/sample/controller/OrderController.java
@@ -0,0 +1,23 @@
+package com.kalsym.sample.controller;
+
+public class OrderController {
+
+    public Object getPrice(String providerConfig, String orderObject, String systemTransactionId, String fulfillment) {
+        return null;
+    }
+
+    public Object placeOrder(String providerConfig, String orderObject, String systemTransactionId) {
+        return null;
+    }
+
+    public String getToken(String systemTransactionId, String url, String username, String password, Integer
+            connectTimeout, Integer waitTimeout) {
+        return null;
+    }
+
+    public Object getAddressId(String systemTransactionId, String address, String url, String key) {
+        return null;
+    }
+
+
+}
